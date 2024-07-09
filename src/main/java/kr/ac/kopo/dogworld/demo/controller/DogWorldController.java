051faf0eeaec37d58eee9a10028f756d87366e55
JungmiP <162236916+JungmiP@users.noreply.github.com>
2024-07-09 21:43:16 +0900

    add login function
    add

--- src/main/java/kr/ac/kopo/dogworld/demo/controller/DogWorldController.java
+++ src/main/java/kr/ac/kopo/dogworld/demo/controller/DogWorldController.java
@@ -4,10 +4,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.servlet.http.HttpServletRequest;
 import kr.ac.kopo.dogworld.demo.jpa.JpaMemberRepository;
@@ -43,14 +42,17 @@ public class DogWorldController {
 	
 	//로그인
 	@RequestMapping(value="/loginForm")
-	public ModelAndView loginForm() {
+	public ModelAndView loginForm(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("loginForm");
+		String referer = request.getHeader("Referer");
+		System.out.println("이전 페이지!!!!" + referer);
+		request.getSession().setAttribute("prevUrl", referer);
 		return mav;
 	}
 	
 	@RequestMapping(value="/login")
-	public ModelAndView login(MemberVO member) {
+	public ModelAndView login(MemberVO member, HttpServletRequest request, RedirectAttributes redirectAttributes) {
 		String loginId = member.getId();
 		String loginPassword = member.getPassword();
 		Optional<MemberVO> loginUser = jpaMember.findById(loginId);
@@ -58,9 +60,16 @@ public class DogWorldController {
 		
 		ModelAndView mav = new ModelAndView();
 		if(loginPassword.equals(password)) {
-			mav.addObject("loginUser", loginUser.get());
+			//mav.addObject("loginUser", loginUser.get());
+			//request.setAttribute("loginId", loginId);
 			//도메인주소
-			mav.setViewName("afterLoginPage");
+			redirectAttributes.addAttribute("id", loginId);
+			redirectAttributes.addFlashAttribute("loginUser", loginUser.get());
+			String prevUrl = (String)request.getSession().getAttribute("prevUrl");
+			mav.setViewName("redirect:" + prevUrl);
+			System.out.println(prevUrl);
+			
+			
 		} else {
 			mav.setViewName("loginForm");
 		}
